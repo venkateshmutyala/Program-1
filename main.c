576d3a63e2e8916d71f813aa22d88daa872cb7b6
venkateshmutyala <99435244+venkateshmutyala@users.noreply.github.com>
2022-04-07 03:49:34 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,59 +1,37 @@
-#include "tools.h"
-
-int main(void) {
-  char characters[26];
-  int counter = 0;
-  int flag = 0;
-
-  printf("abcdefghijkl\n");
-  printf("Venkatesh Mutyala\n");
-  printf("\n");
-
-  char user_input;
-  for(int i = 0; i < 26; i++) {
-    scanf(" %c", &user_input);
-
-    if(user_input == '#')
+#include "tools.hpp"
+#include<cctype>
+using namespace std;
+int comp(char a, string list_of_characters){
+    for(int i=0;i<26;i++){
+        if(list_of_characters[i]==a) // comparing array values with input character
+            return 1;
+    }
+    return 0;
+}
+    
+int main()
+{ 
+    char c;  // initilizing variables
+  long int i=0;  // initilizing variables
+    std::string list_of_characters;
+    cout << "Program 1 (C++): A List Of Letters Used" << endl;
+    // Print your name or names on the second line of the output and leave a blank line after that.
+    string name = "Venkatesh Mutyala"; 
+    cout << name << endl << endl;
+    while(c!='#') // continue loop until # occurs
     {
-      break;
+        cin>>c; // asking user to input character
+       if(isalpha(c)==0) // checking whether char is alphabetic or not
+            continue;
+        if(isalpha(c)==1)
+            c=(char)tolower(c); // change the character case to lower case 
+        if(comp(tolower(c),list_of_characters)==1) // compare character with array values
+            continue;
+        list_of_characters += tolower(c); 
+        
     }
-
-    if(isalpha(user_input))
-    {
-      user_input = tolower(user_input);
-
-      for(int j = 0; j < i; j++)
-      {
-        if(characters[j] == user_input)
-        {
-           flag = 1;
-           break;
-        }
-      }
-
-      if(flag == 0)
-      {
-        characters[i] = user_input;
-        counter++;
-      }
-      else {
-        flag = 0;
-        i--;
-        continue;
-      }
-      
-    }
-    else {
-      i--;
-      continue;
-    }
-
-  }
-
-
-  for(int i = 0; i < counter; i++)
-  {
-    printf("%c ", characters[i]);
-  }
-  return 0;
+    for(int j=0;j<list_of_characters.length();j++)
+        cout<<list_of_characters[j]<<' ';// printing new array values*/
+    cout<<endl;
+    return 0;
 }
